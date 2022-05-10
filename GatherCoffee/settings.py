f32c3c4fa117998ac152e6c4b0202dddbf47d991
LNOI <19521771@gm.uit.edu.vn>
2022-05-10 14:43:31 +0700

    Da

--- GatherCoffee/settings.py
+++ GatherCoffee/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-ox0^m_w1+(wx#81y7u30@4e@o!@cq&7nv&nzclxqqtdli1ykxj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://a084742fa316491c8c78564efcbce9e0-9a075eac-vm-80.vlab2.uit.edu.vn"]
+ALLOWED_HOSTS = ["https://a084742fa316491c8c78564efcbce9e0-9a075eac-vm-80.vlab2.uit.edu.vn","0.0.0.0","127.0.0.1"]
 
 
 # CSRF_TRUSTED_ORIGINS = ['https://a084742fa316491c8c78564efcbce9e0-9a075eac-vm-80.vlab2.uit.edu.vn']

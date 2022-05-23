eb319b8e592cddf855adaca31eba90fd6b533e25
LNOI <19521771@gm.uit.edu.vn>
2022-05-23 14:32:06 +0700

    update

--- GatherCoffee/settings.py
+++ GatherCoffee/settings.py
@@ -78,7 +78,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('13.229.70.190', 6379)],
+            'hosts': [('127.0.0.1', 6379)],
         }
     }
 }

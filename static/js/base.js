d5802d7f701afdc63de092a12ab82d0892f422e6
LNOI <19521771@gm.uit.edu.vn>
2022-05-23 14:28:55 +0700

    update

--- static/js/base.js
+++ static/js/base.js
@@ -10,38 +10,38 @@ const btnlogin=document.querySelector("#btn-login");
 const btnRegister=document.querySelector("#btn-register");
 
 const uname=document.getElementById("uname");
-if(uname&&uname.innerHTML!=""){
-    const username=uname.innerHTML;
-    commonSocket=new WebSocket(
-        'ws://'
-        + window.location.host
-        +'/ws/common/'
-    )
-    commonSocket.onopen=function(e){
+// if(uname&&uname.innerHTML!=""){
+//     const username=uname.innerHTML;
+//     commonSocket=new WebSocket(
+//         'ws://'
+//         + window.location.host
+//         +'/ws/common/'
+//     )
+//     commonSocket.onopen=function(e){
        
-    }
-    commonSocket.onmessage=function(e){
-        const data = JSON.parse(e.data);
-        if(data.username!=uname.innerHTML){
-            noti=`<div class="toast show float-end" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header">
-            <img src="/static/image/avtUser/p1.png" style="width: 15%;" class="rounded me-2" alt="...">
-            <strong class="me-auto">`+data.username+`</strong>
-            <small>11 mins ago</small>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-            </div>
-            <div class="toast-body">
-            `+data.message+` <button type="button "  class="btn btn-warning float-end ">Warning</button>
-            </div>
-            </div>`
-            document.getElementById("notification").innerHTML+=noti;
-        }
+//     }
+//     commonSocket.onmessage=function(e){
+//         const data = JSON.parse(e.data);
+//         if(data.username!=uname.innerHTML){
+//             noti=`<div class="toast show float-end" role="alert" aria-live="assertive" aria-atomic="true">
+//             <div class="toast-header">
+//             <img src="/static/image/avtUser/p1.png" style="width: 15%;" class="rounded me-2" alt="...">
+//             <strong class="me-auto">`+data.username+`</strong>
+//             <small>11 mins ago</small>
+//             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+//             </div>
+//             <div class="toast-body">
+//             `+data.message+` <button type="button "  class="btn btn-warning float-end ">Warning</button>
+//             </div>
+//             </div>`
+//             document.getElementById("notification").innerHTML+=noti;
+//         }
       
-    }
-    commonSocket.onclose = function(e) {
-        console.log('The socket close unexpectadly');
-    }
-}
+//     }
+//     commonSocket.onclose = function(e) {
+//         console.log('The socket close unexpectadly');
+//     }
+// }
 
 
 function InitBase(){
